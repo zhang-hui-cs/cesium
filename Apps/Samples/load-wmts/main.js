95de5e28af808bd273441fb65d1c2425f6afc984
张慧 <715657607@qq.com>
2020-04-07 18:01:51 +0800

    modify it

--- Apps/Samples/load-wmts/main.js
+++ Apps/Samples/load-wmts/main.js
@@ -7,7 +7,6 @@ function initialViewer(element) {
         (112.972 * Math.PI) / 180.0,
         (28.391 * Math.PI) / 180.0
     );
-    // Cesium.Camera.DEFAULT_VIEW_FACTOR = -0.05;
 
     return new Cesium.Viewer(element, {
         baseLayerPicker: false,
@@ -25,15 +24,6 @@ function initialViewer(element) {
                 'https://webst02.is.autonavi.com/appmaptile?style=6&x={x}&y={y}&z={z}',
             maximumLevel: 17
         })
-        // imageryProvider: new Cesium.WebMapTileServiceImageryProvider({
-        //     url:
-        //         'http://t0.tianditu.gov.cn/img_w/wmts?tk=9d2964079a8920b154e7bebe798f80ca',
-        //     layer: 'img',
-        //     style: 'default',
-        //     tileMatrixSetID: 'w',
-        //     format: 'tiles',
-        //     maximumLevel: 18
-        // })
     });
 }
 
@@ -43,17 +33,6 @@ function addWmts(theViewer) {
             url:
                 'http://10.14.3.230:6080/arcgis/rest/services/CityDesignV6/XJXQCSSJBaseMap/MapServer/WMTS',
             layer: 'XJXQCSSJBaseMap',
-            style: 'default',
-            tileMatrixSetID: 'default028mm',
-            format: 'image/png'
-        });
-
-        // theViewer.imageryLayers.addImageryProvider(imageryProvider);
-
-        imageryProvider = new Cesium.WebMapTileServiceImageryProvider({
-            url:
-                'http://10.14.3.230:6080/arcgis/rest/services/CityDesignV6/XJXQCSSJPlanMap_2/MapServer/WMTS',
-            layer: 'XJXQCSSJPlanMap_2',
             style: 'default',
             tileMatrixSetID: 'default028mm',
             format: 'image/png'
@@ -70,17 +49,10 @@ function addArcgisLayer(theViewer) {
                 url: `http://10.14.3.71:6080/arcgis/rest/services/CityDesignV6/XJXQCSSJPlanMap/MapServer`
             })
         );
-
-        theViewer.imageryLayers.addImageryProvider(
-            new Cesium.ArcGisMapServerImageryProvider({
-                url: `http://10.14.3.71:6080/arcgis/rest/services/CityDesignV6/XJXQCSSJDataMap/MapServer`,
-                layers: '68'
-            })
-        );
     }
 }
 
 const viewer = initialViewer('cesiumContainer');
 viewer.camera.flyHome(0);
-addWmts(viewer);
+// addWmts(viewer);
 // addArcgisLayer(viewer);
